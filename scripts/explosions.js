3a66586077a9ff3ef212227f17166b2fb2c87ab9
leakerman <izogid42@gmail.com>
2020-05-04 19:15:08 +0300

    animation changed, code cleaned

--- scripts/explosions.js
+++ scripts/explosions.js
@@ -5,24 +5,24 @@
     const explode = (x, y) => {
         let particles = [];
         let ratio = window.devicePixelRatio;
-        let c = document.createElement('canvas');
-        let ctx = c.getContext('2d');
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext('2d');
 
-        c.style.position = 'absolute';
-        c.style.left = (x - 100) + 'px';
-        c.style.top = (y - 100) + 'px';
-        c.style.pointerEvents = 'none';
-        c.style.width = 200 + 'px';
-        c.style.height = 200 + 'px';
-        c.style.zIndex = 100;
-        c.width = 200 * ratio;
-        c.height = 200 * ratio;
-        document.body.appendChild(c);
+        canvas.style.position = 'absolute';
+        canvas.style.left = (x - 100) + 'px';
+        canvas.style.top = (y - 100) + 'px';
+        canvas.style.pointerEvents = 'none';
+        canvas.style.width = 200 + 'px';
+        canvas.style.height = 200 + 'px';
+        canvas.style.zIndex = 100;
+        canvas.width = 200 * ratio;
+        canvas.height = 200 * ratio;
+        document.body.appendChild(canvas);
 
         for(var i = 0; i < bubbles; i++) {
             particles.push({
-                x: c.width / 2,
-                y: c.height / 2,
+                x: canvas.width / 2,
+                y: canvas.height / 2,
                 radius: r(20, 30),
                 color: colors[Math.floor(Math.random() * colors.length)],
                 rotation: r(0, 360, true),
@@ -34,30 +34,30 @@
             });
         }
 
-        render(particles, ctx, c.width, c.height);
-        setTimeout(() => document.body.removeChild(c), 1000);
+        render(particles, ctx, canvas.width, canvas.height);
+        setTimeout(() => document.body.removeChild(canvas), 1000);
     }
 
     const render = (particles, ctx, width, height) => {
         requestAnimationFrame(() => render(particles, ctx, width, height));
         ctx.clearRect(0, 0, width, height);
 
-        particles.forEach((p, i) => {
-            p.x += p.speed * Math.cos(p.rotation * Math.PI / 180);
-            p.y += p.speed * Math.sin(p.rotation * Math.PI / 180);
+        particles.forEach((particle) => {
+            particle.x += particle.speed * Math.cos(particle.rotation * Math.PI / 180);
+            particle.y += particle.speed * Math.sin(particle.rotation * Math.PI / 180);
 
-            p.opacity -= 0.01;
-            p.speed *= p.friction;
-            p.radius *= p.friction;
-            p.yVel += p.gravity;
-            p.y += p.yVel;
+            particle.opacity -= 0.01;
+            particle.speed *= particle.friction;
+            particle.radius *= particle.friction;
+            particle.yVel += particle.gravity;
+            particle.y += particle.yVel;
 
-            if(p.opacity < 0 || p.radius < 0) return;
+            if(particle.opacity < 0 || particle.radius < 0) return;
 
             ctx.beginPath();
-            ctx.globalAlpha = p.opacity;
-            ctx.fillStyle = p.color;
-            ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, false);
+            ctx.globalAlpha =particle.opacity;
+            ctx.fillStyle = particle.color;
+            ctx.arc(particle.x, particle.y, particle.radius, 0, 2 * Math.PI, false);
             ctx.fill();
         });
 
@@ -67,5 +67,5 @@
     const r = (a, b, c) => parseFloat((Math.random() * ((a ? a : 1) - (b ? b : 0)) + (b ? b : 0)).toFixed(c ? c : 0));
     const explosiveElements =  Array.from( document.querySelectorAll('.explosion'))
     
-    explosiveElements.forEach(element => element.addEventListener('mousemove', e => explode(e.pageX, e.pageY))) 
+    explosiveElements.forEach(element => element.addEventListener('mouseout', event => explode(event.pageX, event.pageY))) 
     
